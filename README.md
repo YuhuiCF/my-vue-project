60470fd0ffe3e642c1f79bb5a5272a57664dc71d
Yuhui Xiong <Yuhui.Xiong@mcon-group.com>
2018-01-20 10:34:09 +0100

    add eslint-google; use v-for to re-write lists

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# my-project
+# my--vue-project
 
 > A Vue.js project
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,80 +3,29 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li>
+      <li
+        v-for="essentialPage in essentialPages"
+        :key="essentialPage.pageLink"
+      >
         <a
-          href="https://vuejs.org"
           target="_blank"
+          :href="essentialPage.pageLink"
         >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
+          {{ essentialPage.pageName }}
         </a>
       </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li>
+      <li
+        v-for="ecosystemPage in ecosystemPages"
+        :key="ecosystemPage.pageLink"
+      >
         <a
-          href="http://router.vuejs.org/"
           target="_blank"
+          :href="ecosystemPage.pageLink"
         >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
+          {{ ecosystemPage.pageName }}
         </a>
       </li>
     </ul>
@@ -85,12 +34,52 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: 'HelloWorld2',
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      ecosystemPages: [
+        {
+          pageLink: 'http://router.vuejs.org/',
+          pageName: 'vue-router',
+        },
+        {
+          pageLink: 'http://vuex.vuejs.org/',
+          pageName: 'vuex',
+        },
+        {
+          pageLink: 'http://vue-loader.vuejs.org/',
+          pageName: 'vue-loader',
+        },
+        {
+          pageLink: 'https://github.com/vuejs/awesome-vue',
+          pageName: 'awesome-vue',
+        },
+      ],
+      essentialPages: [
+        {
+          pageLink: 'https://vuejs.org',
+          pageName: 'Core Docs',
+        },
+        {
+          pageLink: 'https://forum.vuejs.org',
+          pageName: 'Forum',
+        },
+        {
+          pageLink: 'https://chat.vuejs.org',
+          pageName: 'Community Chat',
+        },
+        {
+          pageLink: 'https://twitter.com/vuejs',
+          pageName: 'Twitter',
+        },
+        {
+          pageLink: 'http://vuejs-templates.github.io/webpack/',
+          pageName: 'Docs for this Template',
+        },
+      ],
+      msg: 'Welcome to my modified Vue.js App',
     }
-  }
+  },
 }
 </script>
 
